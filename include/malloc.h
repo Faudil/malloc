d6a50f8bf647686e6bfd80dd469ce6248d46f2f3
faudil.puttilli <faudil.puttilli@epitech.eu>
2018-01-30 16:31:34 +0100

    now malloc alloc pages

--- include/malloc.h
+++ include/malloc.h
@@ -5,7 +5,7 @@
 ** Login   <faudil.puttilli@epitech.eu>
 ** 
 ** Started on  Thu Sep  7 20:10:31 2017 guacamole
-** Last update Tue Jan 23 16:10:26 2018 guacamole
+** Last update Thu Jan 25 18:05:07 2018 guacamole
 */
 
 #ifndef MALLOC_H_
@@ -14,8 +14,8 @@
 # define MAX_ALLOC_SIZE 200000
 # define HEADER_SIZE  sizeof(t_header);
 
-# define GET_HEADER(block) (t_header *)(block - sizeof(t_header))
-# define GET_BLOCK(header) (void *)(header + sizeof(t_header))
+# define GET_HEADER(block) ((t_header *)(block) - 1)
+# define GET_BLOCK(header) ((t_header *)(header) + 1)
 
 #include <stddef.h>
 #include <unistd.h>
@@ -23,6 +23,7 @@
 typedef struct		s_header
 {
 	size_t		size;
+	size_t		id;
 	char		is_free;
 	struct s_header	*next;
 } __attribute__((packed)) t_header;
@@ -33,8 +34,8 @@ typedef	struct		s_info
 	t_header		*end;
 	size_t			page_allocated;
 	size_t			to_fill;
-	long double		nbr_ptr;
-	long double		nbr_free_ptr;
+	long long		nbr_ptr;
+	long long		nbr_free_ptr;
 } __attribute__((packed)) t_info;
 
 void *malloc(size_t);
@@ -45,8 +46,8 @@ void free(void *);
 void split_block(size_t, t_info *, t_header *);
 void *new_block(size_t, t_info *);
 void update_head(t_header *, t_info *);
-void *find_free_memory(size_t , t_info *);
-void *create_new_block(size_t , t_info *);
+t_header *find_free_memory(size_t , t_info *);
+t_header *create_new_block(size_t , t_info *);
 
 void *alloc_page(int, t_info *);
 
